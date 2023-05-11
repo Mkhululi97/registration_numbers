a40131df195467117f5217b9b2104581c7b24c8e
mkhululi <mkhululimtshali@gmail.com>
2023-05-11 12:44:00 +0200

    initial commit

---
+++ functions/RegistrationNumbers.js
@@ -0,0 +1,3 @@
+function RegNumber() {
+  return 2;
+}
---
+++ test/registrationNumbers.tests.js
@@ -0,0 +1,5 @@
+describe("Testing RegistrationNumber Function", function () {
+  it("should return 2", function () {
+    assert.equal(2, 2);
+  });
+});
