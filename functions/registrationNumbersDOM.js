a42c7d89fb75af650755a73b8e25bf3fcc4687c6
mkhululi <mkhululimtshali@gmail.com>
2023-05-18 10:33:42 +0200

    refactored DOM logic, add functionality to the clear btn and added local storage

--- functions/registrationNumbersDOM.js
+++ functions/registrationNumbersDOM.js
@@ -1,7 +1,6 @@
 const errorMsgEl = document.querySelector(".error-msg");
 const inputRegNum = document.querySelector(".text-input");
 const parentEl = document.querySelector(".reg-num-container");
-const dropdown = document.querySelector("dropdown");
 const addBtn = document.querySelector(".add-btn");
 const filterBtn = document.querySelector(".filter-btn");
 const clearBtn = document.querySelector(".clear-btn");
@@ -9,11 +8,33 @@ const selectMenu = document.querySelector("select");
 const numberPlateArr = [];
 const regNum = RegNumbers();
 
-addBtn.addEventListener("click", function () {
-  let firstRegNum = document.querySelector(".first-reg-num");
+const numberPlateFromLocalStorageArr = JSON.parse(
+  localStorage.getItem("number plate")
+);
+if (numberPlateFromLocalStorageArr !== null) {
+  numberPlateFromLocalStorageArr.forEach(function (currentNumberPlate) {
+    if (currentNumberPlate !== null) {
+      createDivElement(currentNumberPlate);
+    }
+  });
+}
+function createDivElement(input) {
+  // create element
+  let childEl = document.createElement("div");
+  // add class to element
+  childEl.classList.add("reg-num");
+  let firstRegNum = document.querySelector(".reg-num");
+  // populate the div element
+  childEl.innerHTML = input;
+  if (input !== undefined) {
+    parentEl.insertBefore(childEl, firstRegNum);
+  }
+}
+function addBtnFunction() {
   regNum.setRegNum(inputRegNum.value);
-  errorMsgEl.innerHTML = regNum.errorMsg();
+  // deal with the errors before calling the validRegNum function.
   if (regNum.errorMsg().length > 0) {
+    errorMsgEl.innerHTML = regNum.errorMsg();
     errorMsgEl.classList.remove("hidden");
     setTimeout(function () {
       errorMsgEl.classList.add("hidden");
@@ -21,41 +42,31 @@ addBtn.addEventListener("click", function () {
   } else {
     errorMsgEl.classList.add("hidden");
   }
-  // create div elements dynamically
-  if (regNum.errorMsg().length < 1) {
-    let numberPlate = regNum.validRegNum();
-
-    //create new element
-    let newNumberPlateEl = document.createElement("div");
-    // populate new element with text from the input
-    newNumberPlateEl.textContent = numberPlate;
-    // add class name
-    newNumberPlateEl.classList.add("reg-num");
-    // push new div element to array
-    numberPlateArr.push(newNumberPlateEl.outerHTML);
-    // adds new div element to localstorage
-    localStorage.setItem("number plate", JSON.stringify(numberPlateArr));
-    // create new div element
-    let finalEl = document.createElement("div");
-    // set new div element with div from localstorage, returning a nested div
-    finalEl.innerHTML = numberPlateArr[numberPlateArr.length - 1]; // always get last element add to the array
-    // appends the actual from localStorage element to the parentEl
-    parentEl.insertBefore(finalEl.firstChild, firstRegNum);
+  let numberPlate = regNum.validRegNum();
+  // push numberplates to number plate arr
+  numberPlateArr.push(numberPlate);
+  // setup local storage
+  localStorage.setItem("number plate", JSON.stringify(numberPlateArr));
+  createDivElement(numberPlate);
+}
+function filterBtnFunction() {
+  let childElArr = Array.from(parentEl.children);
+  childElArr.forEach(function (child) {
+    if (child.innerHTML.startsWith(selectMenu.value)) {
+      child.classList.remove("hidden");
+    } else {
+      child.classList.add("hidden");
+    }
+  });
+}
+function clearBtnFunction() {
+  // remove all child element from the parent element and clears the registration numbers
+  while (parentEl.lastElementChild) {
+    parentEl.removeChild(parentEl.lastElementChild);
   }
-  inputRegNum.value = "";
-});
-
-filterBtn.addEventListener("click", function () {
-  regNum.getRegNumbersMap();
-  if (selectMenu.value !== "") {
-    alert(regNum.selectedTown(selectMenu.value));
-  }
-});
-clearBtn.addEventListener("click", function () {
-  // clear textContent of newNumberPlateEl
-  // removeAll newNumberPlateEl
-  // hide parent element
-  // call the clearMap method
-  // call the clearFilteredTown method
-  // parentEl.textContent = localStorage.getItem("number plate");
-});
+  regNum.clearMap();
+  localStorage.clear();
+}
+addBtn.addEventListener("click", addBtnFunction);
+filterBtn.addEventListener("click", filterBtnFunction);
+clearBtn.addEventListener("click", clearBtnFunction);
